2fed58e862b79f8dd48b2bb52fb9bb44a6da942e
Veronika Javor <veronika.javor@gmail.com>
2024-06-04 23:03:53 +0200

    add angular fire (firestore and storage),

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
+// Angular core libraries
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Application components
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeInfoComponent } from './employee-info/employee-info.component';
+import { NewEmployeeComponent } from './new-employee/new-employee.component';
+
+// exporting routes configuration
+export const routes: Routes = [
+  // ROUTE TO:
+  // if empty
+  {
+    path: '',
+    redirectTo: '/employeeList',
+    pathMatch: 'full',
+  },
+  // if specified path
+  {
+    path: 'employeeList',
+    component: EmployeeListComponent,
+  },
+  // if specified path
+  {
+    path: 'employeeInfo',
+    component: EmployeeInfoComponent,
+  },
+  // if specified path
+  {
+    path: 'newEmployee',
+    component: NewEmployeeComponent,
+  },
+  // wild card option - goes to the list of employees
+  {
+    path: '**',
+    redirectTo: '/employeeList',
+  },
+];
