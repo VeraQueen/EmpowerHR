7cb0d198b86cb0611be141fbf05e0b688e25111e
Veronika Javor <veronika.javor@gmail.com>
2024-06-05 21:26:50 +0200

    added temporary employee data to employee info component; started working on the same component's html file

--- src/app/employees/employee-info/employee-info.component.ts
+++ src/app/employees/employee-info/employee-info.component.ts
@@ -5,8 +5,28 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './employee-info.component.html',
-  styleUrl: './employee-info.component.css'
+  styleUrl: './employee-info.component.css',
 })
 export class EmployeeInfoComponent {
+  employee = {
+    employeeProfilePicUrl: '',
+    name: 'alan',
+    lastName: 'Brick',
+    gender: 'male',
+    birthYear: 1975,
+    department: 'administration',
+    contractType: 'temp',
+    contractValidity: '3 mths',
+    startedWorking: new Date(2024, 5, 10),
+  };
 
+  employeeProfilePicUrl = this.employee.employeeProfilePicUrl;
+  name = this.employee.name;
+  lastName = this.employee.lastName;
+  gender = this.employee.gender;
+  birthYear = this.employee.birthYear;
+  department = this.employee.department;
+  contractType = this.employee.contractType;
+  contractValidity = this.employee.contractValidity;
+  startedWorking = this.employee.startedWorking;
 }
