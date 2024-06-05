1dd0a8e8bbb4a0bfa83573130b57c33fb1fa7c22
Veronika Javor <veronika.javor@gmail.com>
2024-06-05 18:41:14 +0200

    configured employee list layout and the layout of the app, introduced employee start component (filled) to act as placeholder if the employee is not selected

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -1,12 +1,14 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './employee-list.component.html',
-  styleUrl: './employee-list.component.css'
+  styleUrl: './employee-list.component.css',
 })
 export class EmployeeListComponent {
-
+  employees: String[] = ['James Deen', 'Johnny Depp', 'Mark Cain'];
+  onSelectEmployee() {}
 }
